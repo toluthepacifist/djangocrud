8984c22bf1cadc9f69f256abece5a9059be2be1f
Tolu Ajao <tolurere@gmail.com>
2022-06-24 16:31:59 +0100

    commit

--- I4G002709GLM/settings.py
+++ I4G002709GLM/settings.py
@@ -10,20 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$f7zqj8%k^4x#tb29@4g4@)2euj^j5qptax9!4crx7)xc++!%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
